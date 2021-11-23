4f2511e1884ff4d3500af3b60252ebd8c6a5c617
Einar <einar.bratthall@gmail.com>
2021-11-23 19:15:24 +0100

    new example problem

--- 04_Graph_Theory/01_BFS_and_DFS/Example_Flood_Fill.py
+++ 04_Graph_Theory/01_BFS_and_DFS/Example_Flood_Fill.py
@@ -1,66 +1,47 @@
-#not yet complete
+#input setup, not relevant for solution
 def setupInputOutputSublime():
 	import sys
-	sys.stdout = open("C:\\Users\\einar\\OneDrive\\Skrivebord\\output.txt", mode = 'w')
-	sys.stdin = open("C:\\Users\\einar\\OneDrive\\Skrivebord\\intput.txt", mode = 'r')
+	sys.stdout = open("C:\\Temp\\output.txt", mode = 'w')
+	sys.stdin = open("C:\\Temp\\input.txt", mode = 'r')
 setupInputOutputSublime()
 
+#Solution to: https://leetcode.com/problems/flood-fill/
 #code starts here
-from queue import Queue
+    def floodFill(image, sr, sc, newColor):
+        org_color = image[sr][sc]
+        q = []
+        start_cord = (sr, sc)
+        q.append(start_cord)
+    #	for i in q: print(i)
+    #	print("\n")
+        visited = [[False for i in range(len(image[0]))] for _ in range(len(image))]
+    #	for i in visited:
+    #		print (i)
+    #	print("\n")
 
-testGraph = [[1,1,1],[1,1,0],[1,0,1]]
-sr, sc, newColor = 1, 1, 2
-tst = [0 for _ in range(10)]
-print(tst)
+        visited[start_cord[0]][start_cord[1]] = True
 
-def flood_fill_bfs(sr, sc, newColor):
-	org_color = testGraph[sr][sc]
-	q = []
-	start_cord = (sr, sc)
-	q.append(start_cord)
-	for i in q: print(i)
-	visited = [[False for i in range(len(testGraph[0]))] for _ in range(len(testGraph))]
-	for i in visited:
-		print (i)
+        while q:
+            node = q.pop(0)
 
-	visited[start_cord[0]][start_cord[1]] = True
+            neighbours = []
+            for row_shift in range(-1, 2, 2):
+                if node[0] + row_shift >= 0 and node[0] + row_shift <= len(image)-1:
+                    neighbours.append((node[0] + row_shift,node[1]))
+            for col_shift in range(-1,2, 2):
+                    if node[1] + col_shift >= 0 and node[1] + col_shift <= len(image[1])-1:
+                            neighbours.append((node[0],node[1] + col_shift))
 
-	while q:
-		node = q.pop(0)
-		neighbours = get_neighbours(node, testGraph)
+            for neighbour in neighbours:
+                if not visited[neighbour[0]][neighbour[1]]:
+                    if image[neighbour[0]][neighbour[1]] == org_color:
+                        q.append(neighbour)
+                        visited[neighbour[0]][neighbour[1]] = True
 
-		for neighbour in neighbours:
-			if not visited[neighbour[0]][neighbour[1]]:
-				if testGraph[neighbour[0]][neighbour[1]] == org_color:
-					q.append(neighbour)
-					visited[neighbour[0]][neighbour[1]] = True
+        for row in range((len(image))):
+            for col in range(len(image[1])):
+                if visited[row][col] == True:
+                    image[row][col] = newColor
 
-	for row in range((len(testGraph[0]))):
-		for col in range(len(testGraph[1])):
-			if visited[row][col] == True:
-				testGraph[row][col] = newColor
-
-	print(testGraph)
-
-def get_neighbours(node, adj_lst):
-	neighbours = []
-	for row_shift in range(-1, 2, 2):
-		if node[0] + row_shift >= 0 and node[0] + row_shift <= len(adj_lst):
-			neighbours.append((node[0] + row_shift,node[1]))
-	for col_shift in range(-1,2, 2):
-			if node[1] + col_shift >= 0 and node[1] + col_shift <= len(adj_lst[1]):
-					neighbours.append((node[0],node[1] + col_shift))
-	return neighbours
-	
-
-
-
-flood_fill_bfs(sr,sc,newColor)
-
-
-
-for i in range(-1,2, 2):
-	print(i)
-
-print(get_neighbours((0,0),testGraph))
+        return image
 
