1c5979bf151cde264d0b6ea80656c5e148793f43
Einar Bratthall <einar.bratthall@gmail.com>
2022-03-30 11:24:56 +0200

    string construction with dp

--- 05_Dynamic_Programming/06_canConstruct.py
+++ 05_Dynamic_Programming/06_canConstruct.py
@@ -9,33 +9,62 @@ Any element may be used as meny times as neccecary
 """
 
 
-def canConstruct (words, target, mem = {}):
-    if target in mem:
-        return mem[target]
-    if target == "":
+"""
+Note that words cannot be removed from the middle of the string
+because then new ajecencys would be created
+
+#T.C if len(words) = m and targ = n
+tree is n deep and branching factor of m
+T.C O(m^n) function calls. Copying string is O(n), which
+will be done at every node of tree, complexity is then
+T.C O(n*m^n)
+Space O(m^2)
+"""
+
+def canConstruct(words, targ):
+    if targ == "":
         return True
     
-    contains = False
     for w in words:
-        if w in target:
-            contains = True
-    if not contains:
-        return contains
-        
-    for w in words:
-        if w in target:
-            newT = target.replace(w, "")
-            ans =  canConstruct(words, newT)
-            mem[newT] = ans
+        if targ.find(w) == 0:
+            suffix = targ[len(w):]
+            ans = canConstruct(words, suffix)
             if ans:
-                return ans
-        
-
-targ = "abcdef"
-wrd = ["ab", "abc", "cd", "def", "abcd"]
-print(canConstruct(wrd, targ))
-
+                return True
             
+    return False
+
+#with memoization
+#T.C if len(words) = m and targ = n
+#depth of tree n, branching factor m
+#T.C O(m*n^2) S.C O(n^2)
+def canConstructM(words, targ, mem):
+    if mem[len(targ)] != -1: 
+        return mem[len(targ)]
+    if targ == "": 
+        return True
+    
+    for w in words:
+        if targ.find(w) == 0:
+            suff = targ[len(w):] #suffix of the word
+            if canConstructM(words, suff, mem):
+                mem[len(targ)] = True
+                return True
+    
+    mem[len(targ)] = True       
+    return False
+    
+    
+            
+targ = "abcdef"
+wrd = ["cd", "ab", "abc", "def", "abcd"]
+targ1 = "skateboard"
+wrd1 = ["bo", "rd", "ate", "t", "ska", "sk", "boar"]
+#print(targ[2:])
+
+mem = [-1 for _ in range(len(targ1)+1)]
+print(canConstructM(wrd1, targ1,mem))
+
             
 
     
